ba7c1d5ba56928c2320d1e40aef0bdc0624dd1c6
quick6response <romaniv201573@mail.ru>
2023-03-13 21:18:15 +0300

    Интерфейс

---
+++ src/common/interface/parameter.start.interface.ts
@@ -0,0 +1,27 @@
+export class ParameterStartInterface {
+  vk_access_token_settings: string;
+
+  vk_app_id: number;
+
+  vk_are_notifications_enabled: number;
+
+  vk_is_app_user: number;
+
+  vk_is_favorite: number;
+
+  vk_language: string;
+
+  vk_platform: string;
+
+  vk_ref: string;
+
+  vk_ts: string;
+
+  vk_user_id: number;
+
+  sign: string;
+
+  ip: string;
+
+  userAgent: string;
+}
