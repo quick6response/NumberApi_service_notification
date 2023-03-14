0f7d2cef935c52dc9b93423197909a42bbd65346
quick6response <romaniv201573@mail.ru>
2023-03-14 21:46:19 +0300

    Добавили событие

--- src/numbers/numbers.controller.ts
+++ src/numbers/numbers.controller.ts
@@ -1,4 +1,13 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { NumberFindDto } from './dto/number.find.dto';
+import { NumbersService } from './numbers.service';
 
 @Controller('numbers')
-export class NumbersController {}
+export class NumbersController {
+  constructor(private readonly numbersService: NumbersService) {}
+  @MessagePattern('numbers.get')
+  async getInfo(@Payload() data: NumberFindDto) {
+    return this.numbersService.getInfoNumber(data);
+  }
+}
