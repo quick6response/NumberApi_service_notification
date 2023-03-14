631d1ce323d846a1e9febae826899215c3c75b9e
quick6response <romaniv201573@mail.ru>
2023-03-14 21:02:33 +0300

    Убрали логику и перенесли в другой сервис

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,1 @@
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { UsersCreateInterface } from './interface/users.create.interface';
-import { UsersService } from './users.service';
-
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-  @MessagePattern('users.new')
-  usersNew(@Payload() data: UsersCreateInterface) {
-    return this.usersService.notificationNewUser(data);
-  }
-}
+export class UsersController {}
