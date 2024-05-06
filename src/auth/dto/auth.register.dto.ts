d49bf8c91552ba04aa710f9336bd3147ef3c3e55
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-06 23:23:15 +0300

    Убран лишний код

--- src/auth/dto/auth.register.dto.ts
+++ src/auth/dto/auth.register.dto.ts
@@ -1,8 +1,6 @@
 import { IsNumber } from 'class-validator';
 import { ParameterStartDateDto } from '../../common/rabbitmq/types/parameter.start.date.dto';
-import { RabbitmqNotificationKey } from '../../common/rabbitmq/types/rabbitmq.notification.key.type';
 
-const KEY_REGISTER: RabbitmqNotificationKey = 'auth_register_user';
 export class AuthRegisterDto extends ParameterStartDateDto {
   @IsNumber()
   userId: number;
