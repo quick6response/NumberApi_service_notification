23df1cb39991bd8e1a428f4047a89c49ca3bdf1e
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-06 23:15:11 +0300

    Переименован сервис и обновлены импорты

--- src/common/rabbitmq/service/rabbitmq.api.service.ts
+++ src/common/rabbitmq/service/rabbitmq.api.service.ts
@@ -2,20 +2,20 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 import { rabbitNameConfig } from '../config/rabbit.name.config';
-import { RabbitmqNotificationKeyType } from '../types/rabbitmq.notification.key.type';
+import { RabbitmqNotificationEventType } from '../types/rabbitmq.notification.events.type';
 
 @Injectable()
 // TODO написать верную реализацию и переименовать сервис, чтобы он отправлял события в главный сервис API, назвать его нужно как-то иначе, не DONUT, ибо эта штука может работать в разные стороны (возможно)
-export class RabbitmqNotificationService {
+export class RabbitmqApiService {
   constructor(
-    @Inject(rabbitNameConfig.NOTIFICATION)
+    @Inject(rabbitNameConfig.SERVICE_API)
     private readonly client: ClientProxy,
   ) {}
 
   // TODO: Написать возвращаемые типы
   async send<
-    K extends keyof RabbitmqNotificationKeyType,
-    V extends RabbitmqNotificationKeyType[K],
+    K extends keyof RabbitmqNotificationEventType,
+    V extends RabbitmqNotificationEventType[K],
   >(routingKey: K, message: V) {
     return this.client.send<K, V>(routingKey, {
       ...message,
@@ -25,8 +25,8 @@ export class RabbitmqNotificationService {
 
   // Событие
   emit<
-    K extends keyof RabbitmqNotificationKeyType,
-    V extends RabbitmqNotificationKeyType[K],
+    K extends keyof RabbitmqNotificationEventType,
+    V extends RabbitmqNotificationEventType[K],
   >(routingKey: K, message: V): Observable<K> {
     try {
       return this.client.emit<K, V>(routingKey, {
