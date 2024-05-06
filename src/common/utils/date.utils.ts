643f47f09f98e0b25f31716d40fea875c7b3b9fc
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-06 23:23:06 +0300

    Обновлен входной параметр для даты, теперь он может быть числом

--- src/common/utils/date.utils.ts
+++ src/common/utils/date.utils.ts
@@ -1,5 +1,5 @@
 export const dateUtils = {
-  getDateFormatNumber: (date: string, isSeconds = true): string => {
+  getDateFormatNumber: (date: string | number, isSeconds = true): string => {
     return `${dateUtils.convertDateToFormat(
       date,
     )} в ${dateUtils.convertTimeToFormat(date, isSeconds)}`;
@@ -8,7 +8,7 @@ export const dateUtils = {
    * Делаем из даты формат 04.01.2023
    * @param date
    */
-  convertDateToFormat: (date: string): string => {
+  convertDateToFormat: (date: string | number): string => {
     const inputDate = new Date(date);
     const dayInputDate = inputDate.getDate();
     const monthInputDate = inputDate.getMonth() + 1; // месяцы начинаются с 0
@@ -30,7 +30,7 @@ export const dateUtils = {
    * @param date
    * @param isSeconds время с секундами
    */
-  convertTimeToFormat: (date: string, isSeconds = false): string => {
+  convertTimeToFormat: (date: string | number, isSeconds = false): string => {
     const inputDate = new Date(date);
     const minutesInputDate = inputDate.getMinutes();
     const hoursInputDate = inputDate.getHours();
