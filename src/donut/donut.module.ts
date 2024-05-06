ddbcf74182857884a426d1636b38441fa0d5aa3f
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-07 01:00:58 +0300

    Обновлен импорт rabbit на отдельный модуль

--- src/donut/donut.module.ts
+++ src/donut/donut.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { MainApiClientModule } from '../common/rabbitmq/main.api.client.module';
 import { VkHelpModule } from '../vk/vk.help.module';
 import { DonutController } from './donut.controller';
 import { DonutService } from './donut.service';
@@ -9,31 +8,6 @@ import { DonutUpdate } from './donut.update';
 @Module({
   controllers: [DonutUpdate, DonutController],
   providers: [DonutService],
-  imports: [
-    VkHelpModule,
-    ClientsModule.registerAsync([
-      {
-        name: 'DONUT_SERVICE',
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.RMQ,
-          options: {
-            urls: [
-              {
-                hostname: configService.get<string>('RABBITMQ_HOST'),
-                port: configService.get<number>('RABBITMQ_PORT'),
-                password: configService.get<string>('RABBITMQ_DEFAULT_PASS'),
-                username: configService.get<string>('RABBITMQ_DEFAULT_USER'),
-              },
-            ],
-            queueOptions: {
-              durable: true,
-            },
-            queue: 'vk_donut_queue',
-          },
-        }),
-      },
-    ]),
-  ],
+  imports: [VkHelpModule, MainApiClientModule],
 })
 export class DonutModule {}
