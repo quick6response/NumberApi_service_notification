f95fdb8199c77d03172d1251389db2ee91748cbc
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-07 21:56:45 +0300

    Заменено ДТО для событий об активации и деактивации подписки.

--- src/donut/donut.controller.ts
+++ src/donut/donut.controller.ts
@@ -1,18 +1,18 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { DonutService } from './donut.service';
-import { DonutUserDto } from './dto/donut.user.dto';
+import { DonutUserEventDto } from './dto/donut.user.event.dto';
 
 @Controller('donut')
 export class DonutController {
   constructor(private readonly donutService: DonutService) {}
   @MessagePattern('donut_subscriptionIssuance')
-  async subscriptionIssuance(@Payload() data: DonutUserDto) {
+  async subscriptionIssuance(@Payload() data: DonutUserEventDto) {
     return this.donutService.subscriptionIssuance(data);
   }
 
   @MessagePattern('donut_subscriptionExpired')
-  async subscriptionExpired(@Payload() data: DonutUserDto) {
+  async subscriptionExpired(@Payload() data: DonutUserEventDto) {
     return this.donutService.subscriptionExpired(data);
   }
   // todo событие о изменение стоимости подписки
