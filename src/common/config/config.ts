d929118682b112289ecb64baf0b96637730dec31
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-07 21:56:55 +0300

    Удален лишний экспорт

--- src/common/config/config.ts
+++ src/common/config/config.ts
@@ -1,7 +1,7 @@
-import vkChatConfig from './vk.chats.config';
+import { VKChatsEnum } from './vk.chats.config';
 
 export default () => ({
   VK_GROUP_TOKEN: process.env.VK_GROUP_TOKEN,
   VK_GROUP_ID: process.env.VK_GROUP_ID,
-  ...vkChatConfig,
+  ...VKChatsEnum,
 });
