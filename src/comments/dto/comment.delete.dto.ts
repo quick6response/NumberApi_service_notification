dbec72717b29f2f2e4bfb70c5ce2adf2c4b8ae3c
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-09 21:07:58 +0300

    Удалены ДТО и заменены одним ДТо

--- src/comments/dto/comment.delete.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IntersectionType, PickType } from '@nestjs/mapped-types';
-import { ParameterStartDto } from '../../common/dto/parameter.start.dto';
-import { CommentCreateDto } from './comment.create.dto';
-
-export class CommentDeleteDto extends IntersectionType(
-  ParameterStartDto,
-  PickType(CommentCreateDto, ['commentId', 'number'] as const),
-) {}
