d4894785209c883abddcd2e5720a301780bba0bf
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-09 21:07:29 +0300

    Удален файл

--- src/auth/dto/auth.register.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNumber } from 'class-validator';
-import { ParameterStartDateDto } from '../../common/rabbitmq/types/parameter.start.date.dto';
-
-export class AuthRegisterDto extends ParameterStartDateDto {
-  @IsNumber()
-  userId: number;
-}
