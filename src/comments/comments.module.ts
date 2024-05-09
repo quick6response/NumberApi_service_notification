0e01c84846ef9ad62839d68038704c75afb89713
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-09 21:06:47 +0300

    Удален сервис для генерации клавиатуры к комментариям

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { NumbersModule } from '../numbers/numbers.module';
 import { VkHelpModule } from '../vk/vk.help.module';
 import { CommentsController } from './comments.controller';
-import { CommentsKeyboardService } from './comments.keyboard.service';
 import { CommentsService } from './comments.service';
 
 @Module({
   controllers: [CommentsController],
-  providers: [CommentsService, CommentsKeyboardService],
+  providers: [CommentsService],
   imports: [VkHelpModule, NumbersModule],
 })
 export class CommentsModule {}
