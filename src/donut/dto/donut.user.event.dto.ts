e92ebb0fd7e78f34c41859601731923656013ea6
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-09 21:02:16 +0300

    Обновлены ДТО о выдаче подписки ВК донут

--- src/donut/dto/donut.user.event.dto.ts
+++ src/donut/dto/donut.user.event.dto.ts
@@ -1,8 +1,25 @@
-import { ParameterRabbitmqDateDto } from '../../common/dto/parameter.rabbitmq.date.dto';
+export class DonutUserSubscriptionIssuanceEventDto {
+  userId: number;
+  userVkId: number;
+  // был ли пользователь в системе
+  isUser: boolean;
+  // дата начала подписки
+  startDate?: number;
+  // дата окончания подписки
+  endDate?: number;
+  // сумма подписки
+  amount?: number;
+  // цена подписки за месяц
+  price?: number;
+  //
+  date: number;
+}
 
-export class DonutUserEventDto extends ParameterRabbitmqDateDto {
+export class DonutUserEventDto {
   // айди пользователя в сервисе
   userId: number;
   // айди пользователя вк
   userVkId: number;
+  //
+  date: number;
 }
