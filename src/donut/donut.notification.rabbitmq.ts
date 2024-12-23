28cc2aa5e8491054c5cc4f92f992d46fc631f10d
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-12-24 00:50:01 +0300

    Добавлен try catch для обработки событий по уведомлению пользователей о продление/приорибении, окончании подписки. Добавлено сохранение списка сообщений

--- src/donut/donut.notification.rabbitmq.ts
+++ src/donut/donut.notification.rabbitmq.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import {
   MicroservicesEventConstant,
   RabbitmqExchangesConstant,
@@ -10,32 +10,67 @@ import { RabbitmqSubscribeNotificationService } from '../common/rabbitmq/decorat
 
 @Injectable()
 export class DonutNotificationRabbitmq {
+  private readonly logger = new Logger(DonutNotificationRabbitmq.name);
+
   constructor(private readonly donutService: DonutNotificationService) {}
 
   @RabbitmqSubscribeNotificationService({
     exchange: RabbitmqExchangesConstant.mainServiceApi,
     routingKey:
       MicroservicesEventConstant.notification.donut_subscriptionIssuance,
+    queueOptions: {
+      durable: true,
+      autoDelete: false,
+      deadLetterExchange: RabbitmqExchangesConstant.mainServiceApi,
+      deadLetterRoutingKey: `${MicroservicesEventConstant.notification.donut_subscriptionIssuance}.retry`,
+    },
   })
   async subscriptionIssuance(data: DonutUserEventDto) {
-    return await this.donutService.subscriptionIssuance(data);
+    try {
+      await this.donutService.subscriptionIssuance(data);
+    } catch (err) {
+      this.logger.error({ err, extra: { data } });
+      throw err;
+    }
   }
 
   @RabbitmqSubscribeNotificationService({
     exchange: RabbitmqExchangesConstant.mainServiceApi,
     routingKey:
       MicroservicesEventConstant.notification.donut_subscriptionExpired,
+    queueOptions: {
+      durable: true,
+      autoDelete: false,
+      deadLetterExchange: RabbitmqExchangesConstant.mainServiceApi,
+      deadLetterRoutingKey: `${MicroservicesEventConstant.notification.donut_subscriptionExpired}.retry`,
+    },
   })
   async subscriptionExpired(data: DonutUserEventDto) {
-    return await this.donutService.subscriptionExpired(data);
+    try {
+      await this.donutService.subscriptionExpired(data);
+    } catch (err) {
+      this.logger.error({ err, extra: { data } });
+      throw err;
+    }
   }
 
   @RabbitmqSubscribeNotificationService({
     exchange: RabbitmqExchangesConstant.mainServiceApi,
     routingKey:
       MicroservicesEventConstant.notification.donut_subscriptionProlonged,
+    queueOptions: {
+      durable: true,
+      autoDelete: false,
+      deadLetterExchange: RabbitmqExchangesConstant.mainServiceApi,
+      deadLetterRoutingKey: `${MicroservicesEventConstant.notification.donut_subscriptionProlonged}.retry`,
+    },
   })
   async subscriptionProlonged(data: DonutUserEventDto) {
-    return await this.donutService.subscriptionProlonged(data);
+    try {
+      await this.donutService.subscriptionProlonged(data);
+    } catch (err) {
+      this.logger.error({ err, extra: { data } });
+      throw err;
+    }
   }
 }
